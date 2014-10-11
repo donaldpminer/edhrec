046148e8c88b8a362a902fc4939509a4f0bf09cb
Donald Miner <donaldpminer@gmail.com>
2014-10-11 15:03:31 +0000

    added a script that dedups the decks in redis. this will improve performance and now I no longer do deduplication by default in get_decks

---
+++ dedup.py
@@ -0,0 +1,20 @@
+import core
+import json
+
+r=core.get_redis()
+
+strs = r.keys('DECKS_*')
+
+
+for s in strs:
+    ds = core.get_decks(s, dedup=True)
+    dds = core.dedup_decks(ds)
+
+    r.delete('DDD_' + s)
+    for deck in dds:
+        r.rpush('DDD_' + s, json.dumps(deck))
+
+    r.delete('OLD_' + s)
+    r.rename(s, 'OLD_' + s)
+    r.rename('DDD_' + s, s)
+
